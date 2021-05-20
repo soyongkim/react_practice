c69b919bbbbb1bd5ca0216de0bb949a54467f709
jihoon <jihoon9601@gmail.com>
2021-05-20 16:07:47 +0900

    #4 complete

--- src/pages/Registration/index.js
+++ src/pages/Registration/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Signup from './../../components/Signup';
 import './styles.scss'
 
 
@@ -6,9 +7,7 @@ class Registration extends Component {
     render() {
         return (
             <div>
-                <h1>
-                    Registration page
-                </h1>
+                <Signup />
             </div>
         );
     }
