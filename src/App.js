2b5debfdfaa437899b7ef31bd4f619ec8eca1054
smalldragon <thdyd324@gmail.com>
2021-05-20 14:56:32 +0900

    #3 complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { auth, handleUserProfile } from './firebase/utils';
 
 // layouts
 import MainLayout from './layouts/MainLayout';
@@ -9,25 +9,74 @@ import HomepageLayout from './layouts/HomepageLayout';
 // pages
 import Homepage from './pages/Homepage';
 import Resgration from './pages/Registration';
+import Login from './pages/Login';
 import './default.scss';
 
-function App() {
-  return (
-    <div className="App">
+
+const initialState = {
+  currentUser: null
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState
+    };
+  }
+
+  authListner = null;
+
+  componentDidMount() {
+    this.authListner = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+
+      this.setState({
+        ...initialState
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.authListner();
+  }
+
+  render() {
+    const { currentUser} = this.state;
+
+    return (
+      <div className="App">
         <Switch>
           <Route exact path="/" render={() => (
-            <HomepageLayout>
+            <HomepageLayout currentUser={currentUser}>
               <Homepage />
             </HomepageLayout>
           )} />
           <Route path="/registration" render={() => (
-            <MainLayout>
+            <MainLayout currentUser={currentUser}>
               <Resgration />
             </MainLayout>
-          )}  />
+          )} />
+          <Route path="/login" 
+          render={() => currentUser ? <Redirect to="/" /> : (
+              <MainLayout currentUser={currentUser}>
+                <Login />
+              </MainLayout>
+            )} />
         </Switch>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA8-hCHnw5lnmiq6kP4A3TD7jIT14VyYU0",
+    authDomain: "react-ecommerce-website-769e8.firebaseapp.com",
+    projectId: "react-ecommerce-website-769e8",
+    storageBucket: "react-ecommerce-website-769e8.appspot.com",
+    messagingSenderId: "661658113817",
+    appId: "1:661658113817:web:68b03e4c19b693a7e79788"
+  };
